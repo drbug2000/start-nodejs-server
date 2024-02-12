5cef5033c6cecfb23690b85a30e9182cf48f0d09
kim_sang_ june <79149384+drbug2000@users.noreply.github.com>
2024-02-12 22:59:14 +0900

    [add] mypage and edit detail page
    add db user.detail
    add mypage and edit detail page

--- server.js
+++ server.js
@@ -32,7 +32,38 @@ new MongoClient(url).connect().then((client)=>{
     console.log(err)
 })
 
+//passport로 session을 구현하기 위한 library setting
+const session = require('express-session')
+const passport = require('passport')
+const LocalStrategy = require('passport-local')
+app.use(passport.initialize())
+app.use(session({
+	secret: 'kimsessionpasswordisk',
+	resave: false,
+	saveUninitialized: false,
+    cookie : {maxAge: 60*60*1000}
+}))
+app.use(passport.session())
 
+//세션을 만드는 자동 코드
+passport.serializeUser((user,done)=>{
+    //요청.login 이 실행되면, 이 안의 코드도 같이 실행됨
+    console.log(user)//로그인 시도중인 유저의 정보가 담긴다. 
+    //위 passport.use(local)에서 return cb(null, result)했던 result가 그대로 담겨서 그렇다.
+    process.nextTick(()=>{//nodejs에서 특정 코드를 비동기적으로 처리 해주는 문법(queueMicrotasj())
+        //이 내용의 document를 생성하고 쿠키도 알아서 보내줌
+        done(null, {id : user._id , username:user.username})//세션document에 기록할 내용
+        //이렇게 하면 세션이 발행됨. DB연결은 아직 안했으니 메모리에 저장됨
+    })
+})
+
+//사용자가 보낸 쿠키를 분석하는 코드 
+passport.deserializeUser((user,done)=>{
+	//유저가 보낸 쿠키(token)를 분석하는 코드
+	process.nextTick(()=>{
+		done(null, user)		
+	})
+})
 
 //서버의 기능 작성
 app.get('/',(요청,응답)=> {
@@ -55,7 +86,7 @@ app.get('/list',async(요청,응답)=> {
     //응답.send(result[0].title) "응답"은 한번만 해야됨
 
     //ejs 파일 전송 
-    응답.render('list.ejs', {posts : result })//render를 해야 ejs 파일 보내짐
+    응답.render('list.ejs', {posts : result ,user : 요청.user})//render를 해야 ejs 파일 보내짐
     //기본 경로는 views floder로 되어 있음
     //두번째 인자로 데이터를 ejs로 보내야함
 })
@@ -71,7 +102,7 @@ app.get('/time',async(요청,응답)=> {
 app.get('/write',async(요청,응답)=> {
     
     //ejs 파일 전송 
-    응답.render('write.ejs')//render를 해야 ejs 파일 보내짐
+    응답.render('write.ejs',{user:요청.user})//render를 해야 ejs 파일 보내짐
     //기본 경로는 views floder로 되어 있음
     //두번째 인자로 데이터를 ejs로 보내야함
 })
@@ -104,7 +135,7 @@ app.get('/detail/:id',async(요청,응답)=> {
     if(param==null){
         응답.status(404).send("옳지 않은 url 요청")
     }
-    응답.render('detail.ejs',{ post : result })    
+    응답.render('detail.ejs',{ post : result,user: 요청.user })    
     }catch(e){
         console.log(e)
         응답.status(404).send("옳지 않은 url 요청")
@@ -123,7 +154,7 @@ app.get('/edit/:id',async(요청,응답)=> {
         if(param==null){
             응답.status(404).send("옳지 않은 url 요청")
         }else{
-            응답.render('edit_put.ejs',{ post : result })
+            응답.render('edit_put.ejs',{ post : result, user:요청.user })
         }    
     }catch(e){
         console.log(e)
@@ -209,7 +240,114 @@ app.get('/list/:page',async(요청,응답)=> {
     //응답.send(result[0].title) "응답"은 한번만 해야됨
     console.log("doc_count" + doc_count)
     //ejs 파일 전송 
-    응답.render('lists.ejs', { posts : result, pages : page_meta })//render를 해야 ejs 파일 보내짐
+    응답.render('lists.ejs', { posts : result, pages : page_meta ,user:요청.user})//render를 해야 ejs 파일 보내짐
     //기본 경로는 views floder로 되어 있음
     //두번째 인자로 데이터를 ejs로 보내야함
+})
+
+passport.use(new LocalStrategy(async (입력한아이디, 입력한비번,cb)=> { 
+	//유저가 입력한 id-password가 맞는지 검사하는 코드
+	console.log('passport start')
+	//DB조회
+	let result = await db.collection('user').findOne({username : 입력한아이디})
+	if(!result){
+	//id가 없는경우
+        console.log('[login fail]no id')
+		               //회원인증 실패 = false
+		return cb(null,false,{message: '아이디 DB에 없음'})
+	}
+	if (result.password == 입력한비번){
+        console.log('[login]mathch id & password')
+		//로그인 성공
+		return cb(null, result)
+	}else {
+        console.log('[login fail]not match password')
+                   //비밀번호 불일치 = false   
+		return cb(null,false,{message :'비번 불일치'})
+	}
+//전체 코드를 try-catch 하는것도 하면 좋음
+}))
+
+app.get('/login',async(요청,응답)=> {
+    
+    try{
+        응답.render('login.ejs',{user:요청.user})//기능이 끝나면 응답을 해줘야함( 그래야 무한 로딩 안빠진다 )
+    }catch(e){
+        console.log(e)
+        응답.status(500).send('서버 에러남') 
+    }
+    
+    //redirect하면 다른 링크로 보내줌
+})
+
+app.post('/login',async(요청,응답,next)=>{
+	//입력된 값을 대조하는 코드
+	passport.authenticate('local',(error, user, info)=> { 
+		//비교 작업이 끝나면 실행할 코드  
+		//error: 에러 발생시, user:성공시, info:실패 사유
+        console.log('login start')
+		if(error) return 응답.status(500).json(error)
+		if(!user) return 응답.status(401).json(info.message)//우리가 위에 적은 메세지
+		console.log('login not fail')
+		//session을 만들어줌
+		요청.logIn(user,(err)=>{
+			if(err) return next(err)
+			//로그인 성공시 실행하는 코드
+            console.log('login success')
+			응답.redirect('/list/1')
+		})
+	})(요청,응답,next)
+})
+
+app.get('/mypage',async(요청,응답)=> {
+    let result = await db.collection('user').findOne({_id : new ObjectId(요청.user.id)})
+    let user = {username: result.username, detail : result.detail }
+    try{
+        응답.render('mypage.ejs',{user: user})//기능이 끝나면 응답을 해줘야함( 그래야 무한 로딩 안빠진다 )
+    }catch(e){
+        console.log(e)
+        응답.status(500).send('서버 에러남') 
+    }
+    
+    //redirect하면 다른 링크로 보내줌
+})
+
+app.get('/edit_user',async(요청,응답)=> {
+    let result = await db.collection('user').findOne({_id : new ObjectId(요청.user.id)})
+    let user = {username: result.username, detail : result.detail }
+    console.log(user)
+    try{
+        응답.render('edit_user.ejs',{user : user})//기능이 끝나면 응답을 해줘야함( 그래야 무한 로딩 안빠진다 )
+    }catch(e){
+        console.log(e)
+        응답.status(500).send('서버 에러남') 
+    }
+    
+    //redirect하면 다른 링크로 보내줌
+})
+
+app.post('/edit_user',async(요청,응답)=> {
+    console.log('edit user-detail API')
+    try{
+        if(요청.body.user_detail.length >200){
+            응답.send('no title string')
+        }else{
+        if(!요청.user){
+            응답.send('로그인 필요')
+
+        }
+        console.log(요청.user)
+        var target_id = {_id : new ObjectId(요청.user.id)}
+        console.log( 'targetid'+ target_id)
+        var update = { $set:{ detail : 요청.body.user_detail }}
+        await db.collection('user').updateOne(target_id,update)
+        응답.redirect('/list')//기능이 끝나면 응답을 해줘야함( 그래야 무한 로딩 안빠진다 )
+        }
+
+    }catch(e){
+        console.log(e)
+        응답.status(500).send('서버 에러남') 
+    }
+    
+    //redirect하면 다른 링크로 보내줌
 })
